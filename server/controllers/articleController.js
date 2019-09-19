645f59391a6a25766bcc203ab653f75e82f87172
xavioo <rucahoxavi@gmail.com>
2019-09-19 10:32:38 +0200

    feature(server):Employee can delete an article [finish #168497594]

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,6 +1,7 @@
 /* eslint-disable consistent-return */
 import moment from 'moment';
 import articles from '../models/article-data';
+import employees from '../models/employee-data';
 import validateArticle from '../middlewares/validateArticle';
 import response from '../helpers/response';
 
@@ -20,27 +21,62 @@ const createArticle = async (req, res) => {
     response.response(
       res,
       401,
-      'error', 'Administrator are not allowed to create Article',
+      'error', 'Administrator are not allowed to create an Article',
       true,
     );
   } else {
+    const { id } = req.user;
+    const checkUser = employees.find((user) => user.id === id);
+    if (!checkUser) return res.status(400).send('Oops,you must provide your credentails');
+    const { id: authorId, firstName, lastName } = checkUser;
     const {
       title, article,
     } = req.body;
+    const authorName = `${firstName} ${lastName}`;
     const newArticle = {
-      id: articles.length + 1,
+      articleId: articles.length + 1,
       createdOn: moment().format(),
       title,
       article,
+      authorId,
+      authorName,
     };
     articles.push(newArticle);
-
+    const { authorId: _, ...omitData } = newArticle;
     return res.status(201).json({
       status: 201,
       message: 'article successfully created',
-      data: newArticle,
+      data: { ...omitData },
+      // data: newArticle,
     });
   }
 };
 
-export default { createArticle };
+const deleteArticle = async (req, res) => {
+  const { isAdmin } = req.user;
+  if (isAdmin) {
+    response.response(
+      res,
+      401,
+      'error', 'Only Article Author allowed to delete it! ',
+      true,
+    );
+  } else {
+    const { id } = req.params;
+    const article = articles.findIndex((delArticle) => delArticle.articleId === parseInt(id, 10));
+    if (article !== -1) {
+      articles.splice(article, 1);
+      res.status(200).json({
+        status: 200,
+        message: 'article successfully Deleted',
+      });
+    } else {
+      res.status(404).json({
+        status: 404,
+        message: 'article not found',
+      });
+    }
+  }
+};
+
+export default { createArticle, deleteArticle };
--- server/routes/article.js
+++ server/routes/article.js
@@ -4,5 +4,6 @@ import articleController from '../controllers/articleController';
 
 const router = express.Router();
 router.post('/article', userToken, articleController.createArticle);
+router.delete('/articles/:id', userToken, articleController.deleteArticle);
 
 export default router;
