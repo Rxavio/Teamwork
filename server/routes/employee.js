042f433f784e00cf94d5dd9c103fd6fee2670567
xavioo <rucahoxavi@gmail.com>
2019-09-30 13:58:18 +0200

    bug(UI):differ UI folder from others [finish #168835653]

--- server/routes/employee.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import employeeController from '../controllers/employeeController';
-
-const router = express.Router();
-router.post('/auth/signup', employeeController.signUp);
-router.post('/auth/signin', employeeController.signIn);
-
-export default router;
